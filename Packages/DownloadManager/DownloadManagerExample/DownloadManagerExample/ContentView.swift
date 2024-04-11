12d6e0de474ffcd597afcc281883c9cf2cf3853f
ismaelcompsci <ismaelcompsci@gmail.com>
2024-04-11 00:42:54 -0500

    download example app

---
+++ Packages/DownloadManager/DownloadManagerExample/DownloadManagerExample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DownloadManagerExample
+//
+//  Created by Mirna Olvera on 4/10/24.
+//
+
+import DownloadManager
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world dude!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Packages/DownloadManager/DownloadManagerExample/DownloadManagerExample/DownloadManagerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadManagerExampleApp.swift
+//  DownloadManagerExample
+//
+//  Created by Mirna Olvera on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DownloadManagerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
